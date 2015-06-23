bc7ed292d2ab30a56442bb8f4ee7bc0e54ce48ff
ambidextrousTx <ravisinha@my.unt.edu>
2015-06-22 22:01:40 -0500

    Add stubs for various request handlers

--- server/server.go
+++ server/server.go
@@ -6,5 +6,20 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("../ui/")))
+	http.HandleFunc("/edit", EditPageHandler)
+	http.HandleFunc("/save", SavePageHandler)
+	http.HandleFunc("/delete", DeletePageHandler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func EditPageHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("The edit handler"))
+}
+
+func SavePageHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("The save handler"))
+}
+
+func DeletePageHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("The delete handler"))
 }
